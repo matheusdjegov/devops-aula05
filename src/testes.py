85c697499a4048fcfb73f736fbe907f01927a59d
matheusdjegov <42360336+matheusdjegov@users.noreply.github.com>
2018-09-10 21:23:25 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -12,7 +12,7 @@ else:
 			erroInicializar = True
 		else:
 			for elemento in linha:
-				if elemento != '.’:
+				if elemento != '.':
 				erroInicializar = True
 				if erroInicializar:
 					sys.exit(1)
